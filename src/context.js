f0d6f1ff60615dbf5684ff7df3248640c0fd2d6e
Biplab Wagle <biplabwagle@gmail.com>
2022-07-26 09:38:26 -0500

    Adds search functionality

--- src/context.js
+++ src/context.js
@@ -44,11 +44,19 @@ const AppProvider = ({ children }) => {
     dispatch({ type: REMOVE_STORY, payload: id });
   };
 
+  const handleSearch = (query) => {
+    dispatch({ type: HANDLE_SEARCH, payload: query });
+  };
+
+  const handlePage = (value) => {
+    console.log(value);
+  };
+
   useEffect(() => {
     fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`);
   }, [state.query, state.page]);
   return (
-    <AppContext.Provider value={{ ...state, removeStory }}>
+    <AppContext.Provider value={{ ...state, removeStory, handleSearch }}>
       {children}
     </AppContext.Provider>
   );
