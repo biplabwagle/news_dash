983fd55f928299bf74ef05f149a161a96711758a
Biplab Wagle <biplabwagle@gmail.com>
2022-07-26 08:43:45 -0500

    Adds inital fetch from api

--- src/context.js
+++ src/context.js
@@ -15,6 +15,10 @@ const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?';
 //intial state of the application
 const initialState = {
   isLoading: true,
+  hits: [],
+  query: 'react',
+  page: 0,
+  nbPages: 0,
 };
 
 const AppContext = React.createContext();
@@ -24,11 +28,21 @@ const AppProvider = ({ children }) => {
 
   const fetchStories = async (url) => {
     dispatch({ type: SET_LOADING });
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      dispatch({
+        type: SET_STORIES,
+        payload: { hits: data.hits, nbPages: data.nbPages },
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
-    fetchStories();
-  }, []);
+    fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`);
+  }, [state.query, state.page]);
   return (
     <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
   );
